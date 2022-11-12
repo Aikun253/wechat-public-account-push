d061f727199b6dc61e441e145f5bdb67102686d9
Aikun253 <118000852+Aikun253@users.noreply.github.com>
2022-11-12 13:44:21 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '27DssoiWyb-jdsPMzKkD3ZC9oTZM-VIbwUzSjmVs8ao',
+  CALLBACK_TEMPLATE_ID: '	Dkm7v9WIdmsb7glQUYvdeMDdiSsxv_u_NmeX5A-zUbE',
 
   CALLBACK_USERS: [
     {
